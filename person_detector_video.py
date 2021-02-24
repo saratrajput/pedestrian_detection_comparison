2abc7077e9d373a19469c984ea1cbaf31c6c1c37
pattarsuraj <pattarsuraj@gmail.com>
2021-02-24 10:12:38 +0900

    :bug: SPLAB-16 Saving video on Linux

--- person_detector_video.py
+++ person_detector_video.py
@@ -13,8 +13,11 @@ cap = cv2.VideoCapture('vid.mp4')
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frames_per_second = float(cap.get(cv2.CAP_PROP_FPS))
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 1, (frame_height, frame_width))
+#out = cv2.VideoWriter('output.avi', fourcc, 1, (frame_height, frame_width))
+#out = cv2.VideoWriter('output.avi', fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+out = cv2.VideoWriter('output.avi', fourcc, frames_per_second, (int(cap.get(3)), int(cap.get(4))))
 
 ## the output will be written to output.avi
 #out = cv2.VideoWriter(
@@ -42,6 +45,8 @@ while cap.isOpened():
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         # showing the output image
+        #image_write = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+        #out.write(image_write.astype('uint8'))
         out.write(image.astype('uint8'))
         cv2.imshow("Image", image)
         if cv2.waitKey(25) & 0xFF == ord('q'):
